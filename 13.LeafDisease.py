7b9cafbfad3354d95a58e58663ba9b9128a0f898
yathirv <yathirv4@gmail.com>
2024-03-10 09:11:53 +0530

    mar10

--- 13.LeafDisease.py
+++ 13.LeafDisease.py
@@ -5,6 +5,10 @@ from keras.layers import BatchNormalization, Dropout
 
 
 model = Sequential()
+#  ReLU is often used in hidden layers of a neural network for introducing non-linearity
+#  ReLU sets all negative values to zero and leaves positive values unchanged
+
+# kernel_size=(3,3) , pay attention to a small part of the picture (like looking through a small window of size 3x3).
 model.add(Conv2D(32 , kernel_size=(3,3) , activation = 'relu' , input_shape = ( 128 , 128 , 3)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(BatchNormalization())
@@ -32,7 +36,6 @@ model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(128 , activation = 'relu'))
 
-# ReLU is often used in hidden layers of a neural network for introducing non-linearity
 # softmax is used in the output layer for multi-class classification to obtain probability distributions.
 model.add(Dropout(0.3))
 model.add(Dense(25, activation='softmax'))
@@ -49,6 +52,10 @@ testing_datagen = ImageDataGenerator(rescale=1./255)
 
 # Loading and augmenting training and testing datasets:
 
+# Resizes the images to a standard size of 128x128 pixels.
+# During each training iteration, evaluating the performance of the trained model
+# on the testing dataset, it processes 32 images at once
+
 Training_set = training_datagen.flow_from_directory('Leafdata/train',
                                                     target_size= (128,128),
                                                     batch_size= 32,
@@ -57,7 +64,10 @@ Training_set = training_datagen.flow_from_directory('Leafdata/train',
 labels = (Training_set.class_indices)
 print(labels)
 
-Test_set = testing_datagen.flow_from_directory('Leafdata/test',
+# categorical format" refers to representing labels in a format suitable for models that perform multi-class classification.
+# The two common formats for categorical labels are one-hot encoding and integer encoding.
+
+Test_set = testing_datagen.flow_from_directory('Leafdata/val',
                                                     target_size= (128,128),
                                                     batch_size= 32,
                                                     class_mode='categorical')
@@ -72,7 +82,27 @@ model.fit_generator (Training_set,
                      validation_steps= 125)
 
 model_json = model.to_json()
-with open ( "Leafmodel.json" , w) as json_file:
+with open ( "Leafmodel.json" , "w") as json_file:
     json_file.write(model_json)
     model.save_weights("LeafModel.h5")
     print("Model saved to disk")
+
+
+
+    # T H E O R Y
+
+'''
+class_mode='categorical'
+This parameter is set when loading the datasets using the ImageDataGenerator.flow_from_directory method.
+It indicates that the labels in the dataset are provided in one-hot encoded format,
+meaning each class label is represented as a binary vector.
+
+One-Hot Encoding:
+
+In one-hot encoding, each class label is represented as a binary vector.
+The vector has the length of the total number of classes, and all elements are
+set to zero except for the one corresponding to the class label, which is set to one.
+This format is useful when dealing with models that perform multi-class classification,
+such as neural networks with a softmax activation function in the output layer.
+
+'''
